768004fb6ac12f1087caa05ccf324fd0279ed616
ed <dontemailme@dontemailme.com>
2013-07-04 12:35:45 -0700

    start stop app

--- src/spark_config_cluster_sup.erl
+++ src/spark_config_cluster_sup.erl
@@ -19,10 +19,45 @@
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
+start_link(Args) ->
+	start_app(application:start(sasl)),
+	start_app(application:start(os_mon)),
+	start_app(application:start(appmon)),
+	.
+
+stop()->
+	stop_app(application:stop(appmon)),
+	stop_app(application:stop(os_mon)),
+	stop_app(application:stop(sasl)),
+	ok.
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
 init([]) ->
+	Children = [] ,
     {ok, { {one_for_one, 5, 10}, []} }.
 
+start_app(ok)->
+	ok;
+start_app({error, {already_started, App}}
+		when is_atom(App) ->
+	ok;
+start_app({error, {Reason, App}}) ->
+		when is_atom(App) ->
+	{error, {Reason, App}};
+start_app({E, {Reason, App}}) ->
+	{E, {Reason, App}};
+start_app(_)-> {error, badarg}.
+
+stop_app(ok)->
+	ok;
+stop_app({error,{not_started,App}})
+		when is_atom(App)->
+	ok;
+stop_app({error, {Reason, App}}) ->
+		when is_atom(App) ->
+	{error, {Reason, App}};
+stop_app({E, {Reason, App}}) ->
+	{E, {Reason, App}};
+stop_app(_)-> {error, badarg}.
