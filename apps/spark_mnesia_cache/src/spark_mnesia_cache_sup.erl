531eeb9f65c8f0818a1b93e009f55aa28457d071
ed <dontemailme@dontemailme.com>
2013-07-04 13:07:29 -0700

    .p;u?

--- apps/spark_mnesia_cache/src/spark_mnesia_cache_sup.erl
+++ apps/spark_mnesia_cache/src/spark_mnesia_cache_sup.erl
@@ -47,27 +47,27 @@ init(Args) ->
 	Children = [],
     {ok, { {one_for_one, 5, 10}, Children}}.
 
-start_app(ok)->
+ensure_start_app(ok)->
 	ok;
-start_app({error, {already_started, App})
+ensure_start_app({error, {already_started, App})
 		when is_atom(App) ->
 	ok;
-start_app({error, {Reason, App}}) ->
+ensure_start_app({error, {Reason, App}}) ->
 		when is_atom(App) ->
 	{error, {Reason, App}};
-start_app({E, {Reason, App}}) ->
+ensure_start_app({E, {Reason, App}}) ->
 	{E, {Reason, App}};
-start_app(_)-> {error, badarg}.
+ensure_start_app(_)-> {error, badarg}.
 
-stop_app(ok)->
+ensure_stop_app(ok)->
 	ok;
-stop_app({error,{not_started,App}})
+ensure_stop_app({error,{not_started,App}})
 		when is_atom(App)->
 	ok;
-stop_app({error, {Reason, App}}) ->
+ensure_stop_app({error, {Reason, App}}) ->
 		when is_atom(App) ->
 	{error, {Reason, App}};
-stop_app({E, {Reason, App}}) ->
+ensure_stop_app({E, {Reason, App}}) ->
 	{E, {Reason, App}};
-stop_app(_)-> {error, badarg}.
+ensure_stop_app(_)-> {error, badarg}.
 
