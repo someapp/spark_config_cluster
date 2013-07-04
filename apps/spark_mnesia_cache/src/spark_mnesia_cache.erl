001db8045c26c65a4beac2ac81ef641072d15942
ed <dontemailme@dontemailme.com>
2013-07-04 14:01:09 -0700

    4

--- apps/spark_mnesia_cache/src/spark_mnesia_cache.erl
+++ apps/spark_mnesia_cache/src/spark_mnesia_cache.erl
@@ -3,7 +3,22 @@
 -behaviour(gen_server).
 
 
--export([start_link/0, stop/0]).
+-export([start_link/0,
+	 	 stop/0]).
 
--export([init/1,handle_call/3,handle_cast/2,handle_info/2, terminate/2,code_change/2,code_change/3]).
+-export([init/1,
+		 handle_call/3,
+		 handle_cast/2,
+		 handle_info/2,
+		 terminate/2,
+		 code_change/2,
+		 code_change/3]).
+
+-include_lib("lager/include/lager.hrl").
+
+-define(SERVER, ?MODULE).
+-define(SVRNAME(VSN), list_to_atom(atom_to_list(?SERVER)++VSN).
+-define(CONFPATH,"conf").
+
+-record(state,{conf}).
 
--- src/spark_config_cluster.erl
+++ src/spark_config_cluster.erl
@@ -2,7 +2,15 @@
 
 -behaviour(gen_server).
 
--export([start_link/0, stop/0]).
+-export([start_link/0,
+		 stop/0]).
 
--export([init/1,handle_call/3,handle_cast/2,handle_info/2, terminate/2,code_change/2,code_change/3]).
+-export([init/1,
+		 handle_call/3,
+		 handle_cast/2,
+		 handle_info/2,
+		 terminate/2,
+		 code_change/2,
+		 code_change/3]).
+
 
