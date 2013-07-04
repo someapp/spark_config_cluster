0536768fc1d9e46c05abaea45655c871ca1c1008
root <root@chat.dev0.spark.net>
2013-07-04 13:02:17 -0400

    init

---
+++ ebin/spark_config_cluster.app
@@ -0,0 +1,8 @@
+{application,spark_config_cluster,
+             [{description,[]},
+              {vsn,"1"},
+              {registered,[]},
+              {applications,[kernel,stdlib]},
+              {mod,{spark_config_cluster_app,[]}},
+              {env,[]},
+              {modules,[spark_config_cluster_app,spark_config_cluster_sup]}]}.
---
+++ src/spark_config_cluster_app.erl
@@ -0,0 +1,16 @@
+-module(spark_config_cluster_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    spark_config_cluster_sup:start_link().
+
+stop(_State) ->
+    ok.
